0da33e12419658b062caa69b8603b37bc048f6f4
Bryantrejos.h <bryantrejos.h@gmail.com>
2025-10-18 20:12:27 +0200

    update

---
+++ Miproyectoinicial/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+}
+
+//Esto es un comentario fuera de codigo
+/*
+esto es un comentario
+en las lineas que necesites
+ */
+//TODO esto es un todo que sirve para marcar comentarios donde te faltan cosas o donde no has terminado
+
+public static void main(String[] args) {
+    System.out.println("Este es mi primer comentario y proyecto en java");
+    System.out.println("Esta es la segunda linea en java");
+
+}
+
+public void nombreMetodo() {
+
+}
